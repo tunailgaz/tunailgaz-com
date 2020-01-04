063a70fade0a8a032a5acd2791806645048a9603
tunailgaz <tunailgaz@gmail.com>
2020-01-04 15:59:07 +0300

    test deploy 9

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const testEl = document.querySelector('#test');
-    testEl.innerHTML = 'this inserted by js update 8';
+    testEl.innerHTML = 'this inserted by js update 9';
    /* alert('test');*/
 });
