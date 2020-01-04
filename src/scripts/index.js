9f66b377214f670ad085ff1bf59069bfb2434ac5
tunailgaz <tunailgaz@gmail.com>
2020-01-04 15:57:12 +0300

    fake deploy for test purposes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const testEl = document.querySelector('#test');
-    testEl.innerHTML = 'this inserted by js update 7';
+    testEl.innerHTML = 'this inserted by js update 8';
    /* alert('test');*/
 });
