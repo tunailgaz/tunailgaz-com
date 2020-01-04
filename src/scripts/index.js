b91b20a3d054971e4661d9189e6db8cca19bb75b
tunailgaz <tunailgaz@gmail.com>
2020-01-04 15:36:22 +0300

    test update for -> update1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const testEl = document.querySelector('#test');
-    testEl.innerHTML = 'this inserted by js';
+    testEl.innerHTML = 'this inserted by js update1';
    /* alert('test');*/
 });
