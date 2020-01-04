7736d3a20e8bbcd1e7da38e67252fdb3e8d3e6c7
tunailgaz <tunailgaz@gmail.com>
2020-01-04 16:50:18 +0300

    test 20

--- dist/js/app-3465e6c1.js
+++
@@ -1,1 +0,0 @@
-define("./app-3465e6c1.js",[],(function(){"use strict";document.querySelector("#test").innerHTML="inserted by js update 19"}));
---
+++ dist/js/app-be2f2752.js
@@ -0,0 +1,1 @@
+define("./app-be2f2752.js",[],(function(){"use strict";document.querySelector("#test").innerHTML="inserted by js update 20"}));
