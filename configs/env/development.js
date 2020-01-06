e4c284dceb0cd2eb99f1d0ee999ae5c750210e76
tunailgaz <tunailgaz@gmail.com>
2020-01-06 16:49:10 +0300

    fck that config

--- configs/env/development.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const path = require('path');
-require('dotenv').config({path: path.normalize(__dirname + '/../../.env')});
-module.exports = {
-    base_url : `http://localhost:${process.env.PORT}`,
-};
--- configs/env/production.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const path = require('path');
-require('dotenv').config({path: path.normalize(__dirname + '/../../.env')});
-module.exports = {
-    base_url : process.env.BASE_URL,
-};
